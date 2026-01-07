77d921b22e3f4cd3b7ac37d0ab0a3cf1e1b220a1
Andrew Miller <andrew@84em.com>
2026-01-07 07:24:48 -0600

    v1.4.5 - Add Simple Analytics integration

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache-first strategy for offline functionality
  */
 
-const CACHE_NAME = 'takecareofmycats-v1.4.4';
+const CACHE_NAME = 'takecareofmycats-v1.4.5';
 
 const ASSETS_TO_CACHE = [
     '/',
